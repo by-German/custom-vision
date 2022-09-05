762a1a8f2f2e3b9c6ea57cd98a4f5e36ebcc7977
by-German <german280599@gmail.com>
2022-09-05 08:25:43 -0500

    add react mui

---
+++ q
@@ -0,0 +1,23 @@
+[33mcommit 630b1345254bbb086a748d547b05ef4483a53f86[m[33m ([m[1;36mHEAD -> [m[1;32mdevelop[m[33m, [m[1;32mmaster[m[33m)[m
+Author: by-German <german280599@gmail.com>
+Date:   Sat Sep 3 17:27:03 2022 -0500
+
+    adding render img into div"
+
+[33mcommit 22f370f0a6424f798cc72a32fcece96b89269109[m
+Author: by-German <german280599@gmail.com>
+Date:   Sat Sep 3 05:34:21 2022 -0500
+
+    add image file upload
+
+[33mcommit 49973459a09eb4bcc6122431b8dd00a93dabe371[m[33m ([m[1;31morigin/master[m[33m)[m
+Author: by-German <german280599@gmail.com>
+Date:   Sat Sep 3 03:48:00 2022 -0500
+
+    including microsoft customvision-tfjs
+
+[33mcommit 3a01f0baba2fdd54b3b34aafc1905ade098af2f4[m
+Author: by-German <german280599@gmail.com>
+Date:   Fri Sep 2 05:54:56 2022 -0500
+
+    Initialize project using Create React App
--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import { useEffect, useState, useRef } from 'react';
 import * as cvstfjs from '@microsoft/customvision-tfjs';
 import './App.css';
+import {
+  Button,
+  CircularProgress,
+  Box,
+  Drawer,
+  AppBar,
+  Toolbar,
+  List,
+  Divider,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText
+} from '@mui/material';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+const drawerWidth = 300;
 
 function App() {
-  const model                 = useRef(null);
-  const inputRef              = useRef(null);
-  const imageRef              = useRef(null);
-  const [file, setFile]       = useState();
+  const model = useRef(null);
+  const inputRef = useRef(null);
+  const imageRef = useRef(null);
+  const [file, setFile] = useState();
   const [fileURL, setFileURL] = useState('');
-  const [width, setWidth]     = useState(0);
-  const [height, setHeight]   = useState(0);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
 
   useEffect(() => {
     loadModel("model.json")
@@ -39,16 +57,16 @@ function App() {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
     for (let i = 0; i < results[0].length; i++) {
-      const [x1, y1, x2, y2]  = results[0][i];
-      const probability       = results[1][i];
-      const tag               = results[2][i];
+      const [x1, y1, x2, y2] = results[0][i];
+      const probability = results[1][i];
+      // const tag               = results[2][i];
 
       if (probability > 0.15) {
-        const [rectX, rectY]          = [x1 * width, y1 * height];
-        const [rectWith, rectHeight]  = [x2 * width - rectX, y2 * height - rectY]
-      
+        const [rectX, rectY] = [x1 * width, y1 * height];
+        const [rectWith, rectHeight] = [x2 * width - rectX, y2 * height - rectY]
+
         ctx.strokeStyle = 'red'
         ctx.strokeRect(rectX, rectY, rectWith, rectHeight);
       }
@@ -60,41 +78,99 @@ function App() {
     const results = await model.current.executeAsync(data);
     console.log(results)
     showDetectItemsOnCanvas(results);
-  };  
+  };
 
   const onLoadFile = (e) => {
     setFile(e.target.files[0]);
   }
 
   return (
-    <div id="App"> 
-    
-      <div className='box'> 
-        Hello word
-      </div>
+    <Box sx={{ display: 'flex' }}>
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <h3>Vehicule Detection</h3>
+        </Toolbar>
+      </AppBar>
 
-      <div className='img-container'>
-        <img id='image'
-          src={fileURL} 
-          ref={imageRef} 
-          onLoad={onloadImage} 
-        />
-        <canvas id="canvas" 
-          width={width} 
-          height={height} 
-        />
-      </div>
+      <Drawer
+        variant="permanent"
+        anchor="right"
+        sx={{
+          width: { drawerWidth },
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+      >
 
-      <button onClick={predict}>
-        Execute
-      </button>
+        <Toolbar />
+        <Box sx={{ overflow: 'auto' }}>
+          <List>
+            {['Inbox', 'Starred'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
 
-      <input 
-        type="file"
-        ref={inputRef}  
-        onChange={onLoadFile}
-      />      
-    </div>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        {/* start content */}
+        <div id="App">
+
+          <div className='img-container'>
+            <img id='image'
+              src={fileURL}
+              ref={imageRef}
+              onLoad={onloadImage}
+            />
+            <canvas id="canvas"
+              width={width}
+              height={height}
+            />
+          </div>
+
+          <Button variant="contained" onClick={predict}>
+            Execute
+          </Button>
+
+          <input
+            type="file"
+            ref={inputRef}
+            onChange={onLoadFile}
+            
+          />
+
+
+        </div>
+        {/* end content */}
+      </Box>
+    </Box>
   );
 }
 
