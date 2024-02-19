96fb179a1e540555f9a237cdfb8effa287966cde
bygerman <by.german@outlook.com>
2024-02-19 11:54:59 -0500

    Refactor VideoInformation component

--- src/components/Video-information.js
+++ src/components/Video-information.js
@@ -10,34 +10,28 @@ import {
 } from '@mui/material';
 import FileUploadIcon from '@mui/icons-material/FileUpload'
 import TimeToLeaveIcon from '@mui/icons-material/TimeToLeave';
-import TrafficIcon from '@mui/icons-material/Traffic';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 const width = 500;
 const height = 350;
 
 const VideoInformation = forwardRef((props, ref) => {
-  // general
-  const staticTime                            = 20;
-  
   // Controls for video
-  const model                                 = useRef(null);
-  const finishPredicting                      = useRef(false); // control recursion
-  const inputRef                              = useRef(null);
-  const videoRef                              = useRef(null);
-  const setIsPlaying                          = props.setIsPlaying;
-  const canvasRef                             = useRef(null);
-  const canvasValidZone                       = useRef(null);
-  const validArea                             = useRef({x1: 0, y1:0, x2: 0, y2: 0});
-  const [file, setFile]                       = useState();
-  const [fileURL, setFileURL]                 = useState('');
-  const disabledPlayButton                    = props.disabledPlayButton;
-  const previousNumberVehicles                = useRef(0);
-  const [numberVehicles, setNumberVehicles]   = useState(0);
-  const internalThresholdValue                = useRef(0.50);
-  const internalNumberVehicles                = useRef(0);
-  const [greenTime, setGreenTime]             = useState(0);
-  const setVideoChanged                       = props.setVideoChanged;
+  const model = useRef(null);
+  const finishPredicting = useRef(false); // control recursion
+  const inputRef = useRef(null);
+  const videoRef = useRef(null);
+  const setIsPlaying = props.setIsPlaying;
+  const canvasRef = useRef(null);
+  const canvasValidZone = useRef(null);
+  const validArea = useRef({ x1: 0, y1: 0, x2: 0, y2: 0 });
+  const [file, setFile] = useState();
+  const [fileURL, setFileURL] = useState('');
+  const disabledPlayButton = props.disabledPlayButton;
+  const previousNumberVehicles = useRef(0);
+  const [numberVehicles, setNumberVehicles] = useState(0);
+  const internalThresholdValue = useRef(0.50);
+  const internalNumberVehicles = useRef(0);
+  const setVideoChanged = props.setVideoChanged;
 
   useImperativeHandle(ref, () => ({
     playVideo,
@@ -79,13 +73,13 @@ const VideoInformation = forwardRef((props, ref) => {
     let countCurrentVehicles = 0;
 
     for (let i = 0; i < results[0].length; i++) {
-      const [x1, y1, x2, y2]  = results[0][i];
-      const probability       = results[1][i];
+      const [x1, y1, x2, y2] = results[0][i];
+      const probability = results[1][i];
       // const tag            = results[2][i];
 
       if (probability >= internalThresholdValue.current) {
-        const [rectX, rectY]          = [x1 * width, y1 * height];
-        const [rectWith, rectHeight]  = [x2 * width - rectX, y2 * height - rectY]
+        const [rectX, rectY] = [x1 * width, y1 * height];
+        const [rectWith, rectHeight] = [x2 * width - rectX, y2 * height - rectY]
 
         // limit vehicle count detection area
         if (rectX >= validArea.current.x1 && rectX <= validArea.current.x2 && rectY >= validArea.current.y1) {
@@ -104,7 +98,6 @@ const VideoInformation = forwardRef((props, ref) => {
     }
 
     logicCountingVehicles(countCurrentVehicles);
-    semaphoreTimeLogic();
   }
 
   const logicCountingVehicles = (countCurrentVehicles) => {
@@ -114,14 +107,6 @@ const VideoInformation = forwardRef((props, ref) => {
     previousNumberVehicles.current = countCurrentVehicles;
     setNumberVehicles(internalNumberVehicles.current);
   }
-
-  const semaphoreTimeLogic = () => {
-    if (videoRef.current.currentTime >= (staticTime / 2)) { // fix /2 is necessary, video is in 0.5 speed
-      // number of vehicles is equal to semaphore time (set time)
-      setGreenTime(internalNumberVehicles.current * 2);
-      pauseVideo();
-    }
-  };
 
   const predict = async () => {
     const data = videoRef.current;
@@ -140,7 +125,7 @@ const VideoInformation = forwardRef((props, ref) => {
 
   const onClickValidArea = (e) => {
     const canvas = canvasValidZone.current;
-    
+
     validArea.current.x1 = e.clientX - canvas.getBoundingClientRect().left
     validArea.current.y1 = e.clientY - canvas.getBoundingClientRect().top;
     validArea.current.y2 = validArea.current.y1; // horizantal
@@ -148,18 +133,18 @@ const VideoInformation = forwardRef((props, ref) => {
 
   const showValidAreaOnCanvas = (e) => {
     if (e.buttons === 0) return;
-    
+
     const canvas = canvasValidZone.current;
     const ctx = canvas.getContext('2d');
-    
+
     // get position relative to canvas
     validArea.current.x2 = e.clientX - canvas.getBoundingClientRect().left;
-    
+
     ctx.strokeStyle = 'blue'
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.strokeRect(
-      validArea.current.x1, 
-      validArea.current.y1, 
+      validArea.current.x1,
+      validArea.current.y1,
       validArea.current.x2 - validArea.current.x1, // this fixes to drawn as line
       0  // this fixes to draw as line height = 0
     );
@@ -167,7 +152,7 @@ const VideoInformation = forwardRef((props, ref) => {
 
   const playVideo = () => {
     finishPredicting.current = false;
-    videoRef.current.playbackRate  = 0.5; // fix static time in semaphoreTimeLogic
+    videoRef.current.playbackRate = 0.5; // fix static time in semaphoreTimeLogic
     predict();
     videoRef.current.play();
     setIsPlaying(true);
@@ -188,9 +173,8 @@ const VideoInformation = forwardRef((props, ref) => {
     previousNumberVehicles.current = 0;
     setNumberVehicles(0);
     internalNumberVehicles.current = 0;
-    setGreenTime(0);
     clearCanvas();
-    setTimeout(clearCanvas, 100 ); // fix: clear canvas update
+    setTimeout(clearCanvas, 100); // fix: clear canvas update
   }
 
 
@@ -200,23 +184,23 @@ const VideoInformation = forwardRef((props, ref) => {
         <video id='video' muted loop
           src={fileURL}
           ref={videoRef}
-          width={width} 
+          width={width}
           height={height}
         />
         <canvas
           ref={canvasRef}
-          width={width} 
+          width={width}
           height={height}
         />
         <canvas className="interact"
           onMouseMove={showValidAreaOnCanvas}
-          onMouseDown= {onClickValidArea}
+          onMouseDown={onClickValidArea}
           ref={canvasValidZone}
-          width={width} 
+          width={width}
           height={height}
         />
       </div>
-      
+
       <div className="information">
         <Button variant="contained" component="label" startIcon={<FileUploadIcon />}>
           Select File
@@ -228,15 +212,15 @@ const VideoInformation = forwardRef((props, ref) => {
             onChange={onLoadFile}
           />
         </Button>
-        <List disablePadding>
-          {[["Static Time:", staticTime, <AccessTimeIcon />],
-            ["Number of vehicles", numberVehicles, <TimeToLeaveIcon />],
-            ["Dynamic Time:", greenTime, <TrafficIcon />]].map((content, index) => (
-              <ListItem key={index}>
-              <ListItemIcon>{content[2]}</ListItemIcon>
-              <ListItemText primary={content[0]} secondary={content[1]} />
-            </ListItem>
-          ))}
+        <List >
+          <ListItem>
+            <ListItemIcon>
+              <TimeToLeaveIcon />
+            </ListItemIcon>
+            <ListItemText
+              primary="Number of vehicules"
+              secondary={numberVehicles} />
+          </ListItem>
         </List>
       </div>
     </div>
